237b5a52de087a7a90e9f06c0b42df6068990d6f
Lucas Rosario <LucasRosario@users.noreply.github.com>
2022-08-15 17:27:39 -0300

    Finalizando aula 1

--- infrastructure/repository/transaction.go
+++ infrastructure/repository/transaction.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/codeedu/codebank/domain"
 )
@@ -10,13 +11,13 @@ type TransactionRepositoryDb struct {
 	db *sql.DB
 }
 
-func NewTrasactionRepositoryDb(db *sql.DB) *TransactionRepositoryDb {
+func NewTransactionRepositoryDb(db *sql.DB) *TransactionRepositoryDb {
 	return &TransactionRepositoryDb{db: db}
 }
 
 func (t *TransactionRepositoryDb) SaveTransaction(transaction domain.Transaction, creditCard domain.CreditCard) error {
 	stmt, err := t.db.Prepare(`insert into transactions(id, credit_card_id, amount, status, description, store, created_at)
-		values($1, $2, $3, $4, $5, $6, $7)`)
+								values($1, $2, $3, $4, $5, $6, $7)`)
 	if err != nil {
 		return err
 	}
@@ -55,8 +56,8 @@ func (t *TransactionRepositoryDb) updateBalance(creditCard domain.CreditCard) er
 }
 
 func (t *TransactionRepositoryDb) CreateCreditCard(creditCard domain.CreditCard) error {
-	stmt, err := t.db.Prepare(`insert into transactions(id, credit_card_id, amount, status, description, store, created_at)
-		values($1, $2, $3, $4, $5, $6, $7)`)
+	stmt, err := t.db.Prepare(`insert into credit_cards(id, name, number, expiration_month,expiration_year, CVV,balance, balance_limit) 
+								values($1,$2,$3,$4,$5,$6,$7,$8)`)
 	if err != nil {
 		return err
 	}
@@ -78,4 +79,16 @@ func (t *TransactionRepositoryDb) CreateCreditCard(creditCard domain.CreditCard)
 		return err
 	}
 	return nil
+}
+
+func (t *TransactionRepositoryDb) GetCreditCard(creditCard domain.CreditCard) (domain.CreditCard, error) {
+	var c domain.CreditCard
+	stmt, err := t.db.Prepare("select id, balance, balance_limit from credit_cards where number=$1")
+	if err != nil {
+		return c, err
+	}
+	if err = stmt.QueryRow(creditCard.Number).Scan(&c.ID, &c.Balance, &c.Limit); err != nil {
+		return c, errors.New("credit card does not exists")
+	}
+	return c, nil
 }
